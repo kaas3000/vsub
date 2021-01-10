5f72a8fddf2bc48fe24c8f957391ddb85d869343
kaas3000 <k_meliefste@hotmail.nl>
2021-01-10 16:42:35 +0100

    Main view improvements
    * When editing subtitles an indicator is now displayed to show how long
      a subtitle should be max.
    * When switching selecting the next pair of subtitles, they will be
      scrolled into view
    * WIP Active pair of subtitles indicator

--- src/renderer/components/PresentSubtitles.vue
+++ src/renderer/components/PresentSubtitles.vue
@@ -6,19 +6,12 @@
         <span v-shortkey="['arrowdown']" @shortkey="nextSubtitle()"></span>
         <span v-shortkey="['pageup']" @shortkey="previousSong()"></span>
         <span v-shortkey="['pagedown']" @shortkey="nextSong()"></span>
-        <span
-          v-shortkey="['enter']"
-          @shortkey="selectVisibleSongSubtitles()"
-        ></span>
+        <span v-shortkey="['enter']" @shortkey="selectVisibleSongSubtitles()"></span>
         <v-row>
           <v-col>
-            <v-list>
+            <v-list nav>
               <v-subheader>Liederen</v-subheader>
-              <v-list-item-group
-                mandatory
-                v-model="visibleSong"
-                color="primary"
-              >
+              <v-list-item-group mandatory v-model="visibleSong" color="primary">
                 <v-list-item v-for="(song, i) in songs" :key="i" :value="song">
                   <v-list-item-content>
                     {{ song }}
@@ -26,9 +19,7 @@
 
                   <v-list-item-action v-if="isEditingSongList">
                     <v-btn icon>
-                      <v-icon color="grey lighten-1" @click="editSong(song)"
-                        >mdi-pencil</v-icon
-                      >
+                      <v-icon color="grey lighten-1" @click="editSong(song)">mdi-pencil</v-icon>
                     </v-btn>
                     <v-btn icon @click="$store.dispatch('removeSong', song)">
                       <v-icon color="grey lighten-1">mdi-delete</v-icon>
@@ -42,44 +33,38 @@
 
         <v-row>
           <v-col>
-            <v-btn tile text @click="isEditingSongList = !isEditingSongList"
-              >Bewerkmodus</v-btn
-            >
+            <v-btn tile text @click="isEditingSongList = !isEditingSongList">Bewerkmodus</v-btn>
           </v-col>
           <v-col>
-            <v-dialog
-              v-model="isAddingSong"
-              content-class="addSong"
-              @input="handleEditSongPopup"
-            >
+            <v-dialog v-model="isAddingSong" content-class="addSong" @input="handleEditSongPopup">
               <template v-slot:activator="{ on }">
                 <v-btn tile block v-on="on">+ Nieuw Lied</v-btn>
               </template>
 
-              <v-card class="d-flex flex-column h-100">
+              <v-card class="d-flex flex-column">
                 <v-card-title>Ondertitels bewerken</v-card-title>
                 <v-container fluid class="h-100 d-flex flex-column">
-                  <v-text-field
-                    style="flex: 0 0 auto;"
-                    v-model="newSongTitle"
-                    label="Titel"
-                  ></v-text-field>
-                  <v-textarea
-                    outlined
-                    style="flex: 1 1 auto; overflow: auto; padding-top: 1em;"
-                    auto-grow
-                    label="Tekstregels"
-                    type="number"
-                    v-model="newSongLines"
-                  ></v-textarea>
+                  <v-text-field style="flex: 0 0 auto;" v-model="newSongTitle" label="Titel"></v-text-field>
+                  <div style="position: relative; flex: 1 1 auto;">
+                    <div class="subtitleWidthHint" :style="`width: ${subtitleWidthHint}px;`"></div>
+                    <v-textarea
+                      outlined
+                      auto-grow
+                      :style="`padding-top: 1em; font-family: ${subtitleFontFamily};`"
+                      rows="10"
+                      label="Tekstregels"
+                      type="number"
+                      v-model="newSongLines"
+                    >
+                    </v-textarea>
+                  </div>
+
                   <v-row style="flex: 0 0 auto;">
                     <v-col>
                       <v-btn block @click="resetAddingSong">Annuleren</v-btn>
                     </v-col>
                     <v-col>
-                      <v-btn block color="primary" @click="saveSong"
-                        >Opslaan</v-btn
-                      >
+                      <v-btn block color="primary" @click="saveSong">Opslaan</v-btn>
                     </v-col>
                   </v-row>
                 </v-container>
@@ -97,58 +82,81 @@
             <v-col>
               <v-row>
                 <v-list dense class="col-12">
+                  <!-- The first subtitles should always be empty -->
                   <v-list-item
-                    :class="selectedSubtitle == false ? 'active-subtitle' : ''"
+                    :class="(selectedSubtitle == false ? 'active-subtitle' : '') + 'pa-0'"
+                    :ref="selectedSubtitle == false ? 'activeSubtitle' : ''"
                     @click="
                       setSubtitles('', '');
                       selectedSubtitle = [];
                     "
-                    ><v-list-item-content>
-                      <span class="font-italic grey--text grey-darken-4--text"
-                        >lege regel</span
-                      ><br />
-                      <span class="font-italic grey--text grey-darken-4--text"
-                        >lege regel</span
-                      >
-                    </v-list-item-content>
+                  >
+                    <v-list-item-icon style="align-self: center;">
+                      <v-icon large>{{ selectedSubtitle == false ? "mdi-arrow-right" : "" }}</v-icon>
+                    </v-list-item-icon>
+                    <v-card flat width="100%" tile :outlined="selectedSubtitle == false">
+                      <v-list-item-content class="px-2">
+                        <span
+                          :class="
+                            selectedSubtitle == false
+                              ? 'font-italic white--text white-lighten-2--text'
+                              : 'font-italic grey--text grey-darken-4--text'
+                          "
+                          >lege regel</span
+                        ><br />
+                        <span
+                          :class="
+                            selectedSubtitle == false
+                              ? 'font-italic white--text white-lighten-2--text'
+                              : 'font-italic grey--text grey-darken-4--text'
+                          "
+                          >lege regel</span
+                        >
+                      </v-list-item-content>
+                    </v-card>
                   </v-list-item>
 
                   <template v-for="(subtitles, i) in subtitles">
-                    <v-divider
-                      v-if="subtitles.type === 'divider'"
-                      :key="`${songData.title}-${i}`"
-                    ></v-divider>
+                    <v-divider v-if="subtitles.type === 'divider'" :key="`${songData.title}-${i}`"></v-divider>
 
                     <v-list-item
+                      color="primary"
                       v-else
                       :key="`${songData.title}-${i}`"
                       active-class="active"
                       :class="
-                        [songData.title, i].toString() ===
-                        selectedSubtitle.toString()
-                          ? 'active-subtitle'
-                          : ''
+                        [songData.title, i].toString() === selectedSubtitle.toString()
+                          ? 'pa-0 active-subtitle'
+                          : 'pa-0 '
                       "
+                      :ref="[songData.title, i].toString() === selectedSubtitle.toString() ? 'activeSubtitle' : ''"
                       @click="
                         setSubtitles(subtitles.above, subtitles.below);
                         selectedSubtitle = [songData.title, i];
                       "
                     >
-                      <v-list-item-content>
-                        <span
-                          v-if="subtitles.above === ''"
-                          class="font-italic grey--text grey-darken-4--text"
-                          >lege regel</span
-                        >
-                        {{ subtitles.above }}
-                        <br />
-                        <span
-                          v-if="subtitles.below === ''"
-                          class="font-italic grey--text grey-darken-4--text"
-                          >lege regel</span
-                        >
-                        {{ subtitles.below }}
-                      </v-list-item-content>
+                      <v-card
+                        flat
+                        :color="
+                          [songData.title, i].toString() === selectedSubtitle.toString()
+                            ? 'green white--text'
+                            : 'transparent'
+                        "
+                        width="100%"
+                        tile
+                      >
+                        <v-list-item-content class="px-2">
+                          <span v-if="subtitles.above === ''" class="font-italic grey--text grey-darken-4--text"
+                            >lege regel</span
+                          >
+                          <span v-else> {{ subtitles.above }} </span>
+                          <br />
+                          <span v-if="subtitles.below === ''" class="font-italic grey--text grey-darken-4--text"
+                            >lege regel</span
+                          >
+                          <span v-else> {{ subtitles.below }} </span>
+                        </v-list-item-content>
+                      </v-card>
                     </v-list-item>
                   </template>
                 </v-list>
@@ -192,6 +200,14 @@ export default {
 
     subtitles() {
       return this.getSubtitles(this.songData);
+    },
+
+    subtitleWidthHint() {
+      return this.$store.state.Settings.subtitleEditorWidthHint;
+    },
+
+    subtitleFontFamily() {
+      return this.$store.state.Settings.subtitleEditorFontFamily;
     },
   },
   methods: {
@@ -315,6 +331,11 @@ export default {
         const newSubtitles = subtitles[newIndex];
         this.setSubtitles(newSubtitles.above, newSubtitles.below);
       }
+
+      this.$refs.activeSubtitle[0].$el.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
     },
     nextSubtitle() {
       const [song = this.visibleSong, index = -1] = this.selectedSubtitle;
@@ -335,6 +356,11 @@ export default {
 
       const newSubtitles = subtitles[newIndex];
       this.setSubtitles(newSubtitles.above, newSubtitles.below);
+
+      this.$refs.activeSubtitle[0].$el.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
     },
 
     selectVisibleSongSubtitles() {
@@ -364,23 +390,18 @@ export default {
 
 <style lang="scss" scoped>
 .active-subtitle {
-  &::before {
-    background-color: currentColor;
-    bottom: 0;
-    content: "";
-    left: 0;
-    opacity: 0.1;
-    pointer-events: none;
-    position: absolute;
-    right: 0;
-    top: 0;
-    transition: 0.3s cubic-bezier(0.25, 0.8, 0.5, 1);
-  }
 }
 </style>
 
 <style lang="scss">
 .addSong {
   height: 100%;
+}
+
+.subtitleWidthHint {
+  position: absolute;
+  height: calc(100% - 3em);
+  top: 1em;
+  border-right: 1px solid #ff0000;
 }
 </style>
