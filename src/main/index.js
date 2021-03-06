bc4e7f5399a1dbd228d642e74d1aaa68f3a2d2b1
kaas3000 <k_meliefste@hotmail.nl>
2021-03-06 14:25:29 +0100

    Remember window position and size between restarts

--- src/main/index.js
+++ src/main/index.js
@@ -15,25 +15,46 @@ Store.initRenderer();
 let mainWindow;
 const winURL = process.env.NODE_ENV === "development" ? "http://localhost:9080" : `file://${__dirname}/index.html`;
 
+const parameterStore = new Store({ name: "window-parameters" });
+function loadWindowParameters() {
+  const { width, height } = parameterStore.get("dimensions", { width: null, height: null });
+  const { x, y } = parameterStore.get("location", { x: null, y: null });
+
+  return { x, y, width, height };
+}
+
 function createWindow() {
   /**
    * Initial window options
    */
+  const { x, y, width, height } = loadWindowParameters();
+
   mainWindow = new BrowserWindow({
-    height: 563,
-    useContentSize: true,
-    width: 1000,
+    x,
+    y,
+    width,
+    height,
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true,
     },
   });
 
+  function storeWindowParameters() {
+    const { x, y, width, height } = mainWindow.getBounds();
+
+    parameterStore.set("dimensions", { width, height });
+    parameterStore.set("location", { x, y });
+  }
+
   mainWindow.loadURL(winURL);
 
   mainWindow.on("closed", () => {
     mainWindow = null;
   });
+
+  mainWindow.on("resize", () => storeWindowParameters());
+  mainWindow.on("moved", () => storeWindowParameters());
 }
 
 app.on("ready", () => {
